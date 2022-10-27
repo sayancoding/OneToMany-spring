3007b8ea1ce73106f4854ef3dc86ec0f7b3e66b1
sayancoding <sayanmaity007@gmail.com>
2022-10-27 11:38:45 +0530

    comment unused

--- src/main/java/com/practice/bookapp/entity/Author.java
+++ src/main/java/com/practice/bookapp/entity/Author.java
@@ -12,7 +12,7 @@ import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+//import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 
